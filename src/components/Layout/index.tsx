a846e014c7f81047983d6f8cfe4fc9608ad47a82
MaximianoEduardo <edu.maxi.96@gmail.com>
2020-09-14 18:43:29 -0300

    🚧 React Router 🚧

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,29 @@ import React from 'react'
 import Sidebar from '../Sidebar'
 import Buttons from '../Buttons'
 import { Container } from './styles'
+import Home from '../Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
 
 const Layout: React.FC = () => {
   return (
     <Container>
-      <Sidebar />
-      <Buttons />
+      <Router>
+        <Sidebar />
+          <Switch>
+           <Redirect exact from="/" to="/home" />
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/buttons">
+              <Buttons />
+            </Route>
+          </Switch>
+      </Router>
     </Container>
   );
 };
